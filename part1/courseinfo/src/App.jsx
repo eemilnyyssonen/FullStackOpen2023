e5ff4f33acdef835fe2152b6cd5b29d36f696829
Eemil <80176357+eemilnyyssonen@users.noreply.github.com>
2023-10-31 16:54:12 +0200

    courseinfo

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,47 @@
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
+
+const Content = ({ parts }) => (
+  <div>
+    <Part part={parts[0]}/>
+    <Part part={parts[1]}/>
+    <Part part={parts[2]}/>
+  </div>
+)
+
+const Total = ({ parts }) => {
+  const total = parts.map(part => part.exercises).reduce((tot, cur) => tot + cur)
+  return(
+    <p>Number of exercises {total}</p>
+  )
+}
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  )
+}
+
+export default App
